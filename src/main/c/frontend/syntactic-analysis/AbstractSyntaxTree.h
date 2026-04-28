e2bc882f293c79b83af9231a7fd8615ee5dd6059
marian-24 <125489006+marian-24@users.noreply.github.com>
2026-04-27 22:57:51 -0300

    esqueleto de ABS

--- src/main/c/frontend/syntactic-analysis/AbstractSyntaxTree.h
+++ src/main/c/frontend/syntactic-analysis/AbstractSyntaxTree.h
@@ -3,6 +3,7 @@
 
 #include "../../support/logging/Logger.h"
 #include "../../support/type/ModuleDestructor.h"
+#include "../../support/type/TokenLabel.h"
 #include <stdlib.h>
 
 /** Initialize module's internal state. */
@@ -14,18 +15,50 @@ ModuleDestructor initializeAbstractSyntaxTreeModule();
  * person, but without the madness).
  */
 
-typedef enum ExpressionType ExpressionType;
+/*typedef enum ExpressionType ExpressionType;
 typedef enum FactorType FactorType;
 
 typedef struct Constant Constant;
 typedef struct Expression Expression;
 typedef struct Factor Factor;
 typedef struct Program Program;
+*/
+
+typedef struct Program Program;
+typedef struct StatementList StatementList;
+typedef struct Statement Statement;
+
+//species
+typedef struct SpeciesDefinition SpeciesDefinition;
+typedef struct SpeciesAttributeList SpeciesAttributeList;
+typedef struct SpeciesAttribute SpeciesAttribute;
+typedef struct EnvTolerance EnvTolerance;
+typedef struct RangeValue RangeValue;
+
+//Region
+
+//Ecosystem
+
+//Population
+
+//Encounter
+
+//Simulation
+
+//Control flow
+
+//log
+
+//expressions
+
+//attribute modificaion
+
 
 /**
  * Node types for the Abstract Syntax Tree (AST).
  */
-
+//enums for node types
+/*
 enum ExpressionType {
 	ADDITION,
 	DIVISION,
@@ -65,14 +98,57 @@ struct Expression {
 struct Program {
 	Expression * expression;
 };
+*/
+
+//expression node
 
 /**
  * Node recursive super-duper-trambolik-destructors.
  */
 
-void destroyConstant(Constant * constant);
+ //condition node
+
+
+ //range value
+
+ //environmental
+
+ //species
+
+ //region
+
+ //ecosystem
+
+ //attribute
+
+ //statement
+
+ //on encounter
+
+ //on generation
+ 
+ //every
+
+ //simulate
+
+ //if
+
+ //while
+
+ //foreach
+
+ //log
+
+ //program (root node)
+
+ //destructors
+
+/*void destroyConstant(Constant * constant);
 void destroyExpression(Expression * expression);
 void destroyFactor(Factor * factor);
 void destroyProgram(Program * program);
+*/
+
+
 
 #endif
