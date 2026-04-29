8b3fb658b3516e7a70e0bed7971703cb8befce12
marian-24 <125489006+marian-24@users.noreply.github.com>
2026-04-28 23:50:18 -0300

    cambios

--- src/main/c/support/type/CompilerState.h
+++ src/main/c/support/type/CompilerState.h
@@ -1,6 +1,8 @@
 #ifndef COMPILER_STATE_HEADER
 #define COMPILER_STATE_HEADER
 
+#include "../../frontend/syntactic-analysis/AbstractSyntaxTree.h"
+
 /**
  * The global state of the compiler. Should transport every data structure
  * needed across the different phases of a compilation.
@@ -9,14 +11,15 @@ typedef struct {
 	/**
 	 * The root node of the AST.
 	 */
-	void * abstractSyntaxtTree;
+	Program * abstractSyntaxtTree;
 
 	/**
-	 * The computed value of the entire program (only for the calculator). You
-	 * should change or remove this field, or a random child will die, and it
-	 * will be your fault.
+	 * You should change or remove this field, or a random child will die, and it
+	 * will be your fault. !!
+	 * 
+	 * Set to false by the semantic analysis phase if any error is found.
 	 */
-	signed int value;
+	int succeeded;
 
 	// TODO: Add a symbol table.
 	// TODO: Add an stack to handle nested scopes.
